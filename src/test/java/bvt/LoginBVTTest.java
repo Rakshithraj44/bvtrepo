26c9fb6e55cd0432e4d465ef2b0e985c1553e376
Ubuntu <ubuntu@ip-172-31-12-117.ap-south-1.compute.internal>
2025-07-23 09:30:43 +0000

    Add basic BVT test case

--- src/test/java/bvt/LoginBVTTest.java
+++ src/test/java/bvt/LoginBVTTest.java
@@ -1,11 +1,14 @@
 package bvt;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class LoginBVTTest {
+
     @Test
-    public void testLogin() {
-        System.out.println("BVT login test - Passed");
+    public void testLoginMessage() {
+        String message = "Hello World! It's me Rakshith Raj";
+        assertTrue(message.contains("Rakshith"));
     }
 }
 
